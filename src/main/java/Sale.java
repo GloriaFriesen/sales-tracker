c7c5eaeee435a96c8ef24adf84002709e86a580b
Chris Finney and Gloria Friesen <gloria@>
2017-04-04 14:17:42 -0700

    add Sale class instantiation test, add and test equals override and save method

--- src/main/java/Sale.java
+++ src/main/java/Sale.java
@@ -8,9 +8,63 @@ import java.util.TimerTask;
 import java.sql.Timestamp;
 
 public class Sale {
+  private int item_id;
+  private Timestamp time_sold;
+  private int id;
 
-  public Sale() {
-
+  public Sale(int item_id, Timestamp time_sold) {
+    this.item_id = item_id;
+    this.time_sold = time_sold;
+    this.id = id;
   }
 
+  public int getItemId() {
+    return item_id;
+  }
+
+  public Timestamp getTimeSold() {
+    return time_sold;
+  }
+
+  @Override
+  public boolean equals(Object otherSale) {
+    if (!(otherSale instanceof Sale)) {
+      return false;
+    } else {
+      Sale newSale = (Sale) otherSale;
+      return this.getItemId() == (newSale.getItemId());
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO sales (item_id, time_sold) VALUES (:item_id, :time_sold)";
+      this.id = (int) con.createQuery(sql, true)
+      .addParameter("item_id", this.item_id)
+      .addParameter("time_sold", this.time_sold)
+      .executeUpdate()
+      .getKey();
+    }
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public static List<Sale> all() {
+    String sql = "SELECT * FROM sales";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Sale.class);
+    }
+  }
+
+  public static Sale find(int id) {
+    try (Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM sales WHERE id=:id";
+      Sale sale = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Sale.class);
+      return sale;
+    }
+  }
 }
--- src/test/java/SaleTest.java
+++ src/test/java/SaleTest.java
@@ -11,7 +11,42 @@ import java.sql.Timestamp;
 
 public class SaleTest {
 
-  @Rule
-  public DatabaseRule databse = new DatabaseRule();
+  Timestamp rightNow = new Timestamp(new Date().getTime());
 
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void Sale_instantitatesCorrectly_true() {
+    Sale testSale = new Sale(1, rightNow);
+    assertEquals(true, testSale instanceof Sale);
+  }
+
+  @Test
+  public void Sale_instantitatesWithTimeSold_Timestamp() {
+    Sale testSale = new Sale(1, rightNow);
+    assertEquals(rightNow, testSale.getTimeSold());
+  }
+
+  @Test
+  public void equals_returnsTrueIfPropertiesAreSame() {
+    Sale firstSale = new Sale(1, rightNow);
+    Sale secondSale = new Sale(1, rightNow);
+    assertTrue(firstSale.equals(secondSale));
+  }
+
+  @Test
+  public void save_returnsTrueIfAllTheSame() {
+    Sale testSale = new Sale(1, rightNow);
+    testSale.save();
+    assertTrue(Sale.all().get(0).equals(testSale));
+  }
+
+  @Test
+  public void save_assignsIdToSale() {
+    Sale testSale = new Sale(1, rightNow);
+    testSale.save();
+    Sale savedSale = Sale.all().get(0);
+    assertEquals(testSale.getId(), savedSale.getId());
+  }
 }
